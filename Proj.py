865a65cd6b0cc4a9b8f5cb8af73cfd8011d591de
Daniel Eloy <ist1114609@tecnico.ulisboa.pt>
2026-01-07 15:56:50 +0000

    semWrongAns

--- Proj.py
+++ Proj.py
@@ -8,13 +8,13 @@ def calcula(equipas, tabela, quadro, equipa_win):
 
     for (i, j), nj in quadro.items():
         if nj > 0:
-            w = LpVariable(f"w_{i}_{j}_{equipa_win}", 0, nj, cat="Integer")
-            d = LpVariable(f"d_{i}_{j}_{equipa_win}", 0, nj, cat="Integer")
-            l = LpVariable(f"l_{i}_{j}_{equipa_win}", 0, nj, cat="Integer")
+            w = LpVariable(f"w_{i}_{j}_{equipa_win}", 0, nj, cat='Integer')
+            d = LpVariable(f"d_{i}_{j}_{equipa_win}", 0, nj, cat='Integer')
+            l = LpVariable(f"l_{i}_{j}_{equipa_win}", 0, nj, cat='Integer')
 
             variaveis_jogo[(i,j)] = (w, d, l)
 
-            prob += (w + d + l == nj)
+            prob += w + d + l == nj
 
             if (i == equipa_win):
                 total_vitorias_precisas.append(w)
@@ -22,9 +22,16 @@ def calcula(equipas, tabela, quadro, equipa_win):
                 total_vitorias_precisas.append(l)
 
     if (len(total_vitorias_precisas) == 0):
-        return -1
+        for e in range(1, equipas+1):
+            if(equipa_win == e):
+                continue
+            elif(tabela[equipa_win] < tabela[e]):
+                return -1
     
-    prob += lpSum(total_vitorias_precisas)
+    if total_vitorias_precisas:
+        prob += lpSum(total_vitorias_precisas)
+    else:
+        prob += 0
 
     pontuacao_final = {}
     for e in range(1, equipas+1):
@@ -40,11 +47,11 @@ def calcula(equipas, tabela, quadro, equipa_win):
 
     for e in range(1, equipas+1):
         if e != equipa_win:
-            prob += (pontuacao_final[e] <= pontuacao_final[equipa_win])
+            prob += (pontuacao_final[equipa_win] >= pontuacao_final[e])
 
     status = prob.solve(GLPK(msg=0))
 
-    if (status != 1):
+    if status != 1:
         return -1
     
     return int(value(prob.objective))
@@ -59,8 +66,6 @@ x = equipasjogos.split()
 equipas = int(x[0])
 jogosJogados = int(x[1])
 
-jogostotais = (equipas-1)*2
-
 pontosAtuais = {i:0 for i in range(1, equipas+1)}
 
 jogos = []
@@ -68,7 +73,8 @@ jogos = []
 for la in range(jogosJogados):
     jogojogado = input()
     y = jogojogado.split()
-    u = (int(y[0]), int(y[1]), int(y[2]))
+    casa, fora, vencedor = int(y[0]), int(y[1]), int(y[2])
+    u = (casa, fora, vencedor)
     jogos.append(u)
 
 jogosFeitos = {}
